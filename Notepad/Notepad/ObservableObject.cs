ba617c3d4e1cbcdfe52654fe571f55caae3e8026
Minkyu Choi <devmk1090@gmail.com>
2021-09-09 16:14:16 +0900

    Models 와 ViewModel 연결 코딩중 레이아웃 코딩 진행중

---
+++ Notepad/Notepad/ObservableObject.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Notepad
+{
+    //view(xaml) 와 model 을 바인딩을 시켜서 화면에 보여주게 되는데, 바인딩된 값이 바뀔때마다
+    //변화를 인지하여 백단의 값을 자동으로 바뀌게 해주는 역할
+    public class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged<T>(ref T property, T value, [CallerMemberName] String propertyName = "")
+        {
+            property = value;
+            var handler = PropertyChanged;
+            if (handler != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
---
+++ Notepad/Notepad/ViewModels/MainViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Notepad.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Notepad.ViewModels
+{
+    public class MainViewModel
+    {
+        private DocumentModel _document;
+        public EditorViewModel Editor { get; set; }
+
+        public MainViewModel()
+        {
+            _document = new DocumentModel();
+            Editor = new EditorViewModel(_document);
+        }
+    }
+}
